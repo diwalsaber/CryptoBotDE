0e67327bf44c07a9b2095f9bb8852c5ba938cec4
Diwal <walid.sc@gmail.com>
2023-06-08 10:11:40 +0200

    nouvelle branche pour la version docker du projet

--- data_collectors/start_websocket_and_recover_missing_history.py
+++
@@ -1,8 +0,0 @@
-from data_collectors.history_data_collector_api import download_missing_history_data
-from data_collectors.realtime_data_collector import download_realtime_data
-
-# start downloading realtime data with websocket
-download_realtime_data()
-
-# download today missing data
-download_missing_history_data()
