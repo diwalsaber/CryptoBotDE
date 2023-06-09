e86bc971234aace0599c0343b7e9f9726679c7c7
Diwal <walid.sc@gmail.com>
2023-06-09 17:50:55 +0200

    ajout script bash execution application

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Cloner le dépôt
+git clone https://github.com/diwalsaber/CryptoBotDE.git
+
+# Se déplacer dans le répertoire du projet
+cd CryptoBotDE
+
+# Créer la variable d'environnement AIRFLOW_UID
+echo -e "AIRFLOW_UID=$(id -u)" > .env
+
+# Créer les répertoires nécessaires pour les volumes montés
+mkdir -p ./airflow/logs ./airflow/plugins ./airflow/config
+
+# Construire et lancer les conteneurs Docker
+docker-compose up -d
